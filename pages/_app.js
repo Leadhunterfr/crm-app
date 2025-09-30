562ced8154208028a894cd41a736a15caac96663
Leadhunterfr <leadhunterfr@gmail.com>
2025-09-30 10:40:22 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import "@/styles/globals.css";
+import Layout from "@/components/Layout";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  // Si une page veut désactiver le layout, on check une propriété spéciale
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
+
+  return getLayout(<Component {...pageProps} />);
 }
