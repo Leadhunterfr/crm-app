bb0cd36d755ca38f4881ecd7b2e3115f81c74ad9
Leadhunterfr <leadhunterfr@gmail.com>
2025-10-01 15:02:34 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 export default function MyApp({ Component, pageProps }) {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+
   // Si une page veut désactiver le layout, on check une propriété spéciale
   const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      {getLayout(<Component {...pageProps} />)}
+    </SessionContextProvider>
+  );
 }
